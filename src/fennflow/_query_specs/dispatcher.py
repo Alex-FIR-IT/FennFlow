90981d59acadaff0fde2b49d9c8307c6576c5cf3
admin <osintfir@gmail.com>
2026-05-21 13:05:15 +0300

    chore: specify return type

--- src/fennflow/_query_specs/dispatcher.py
+++ src/fennflow/_query_specs/dispatcher.py
@@ -1,20 +1,24 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Generic
+
+from fennflow._query_specs.base import ReturnType
 
 if TYPE_CHECKING:
+    from collections.abc import Mapping
+
     from fennflow._query_specs.base import BaseQuerySpec
     from fennflow._query_specs.protocols import HasRunMethodProtocol
 
 
-class Dispatcher:
+class Dispatcher(Generic[ReturnType]):
     def __init__(
         self,
-        registry: dict[type[BaseQuerySpec], HasRunMethodProtocol],
+        registry: Mapping[type[BaseQuerySpec[ReturnType]], HasRunMethodProtocol],
     ) -> None:
         self.registry = registry
 
-    async def dispatch(self, query_spec: BaseQuerySpec) -> Any:
+    async def dispatch(self, query_spec: BaseQuerySpec[ReturnType]) -> ReturnType:
         if type(query_spec) not in self.registry:
             raise KeyError(f"Query spec {type(query_spec)=} not found in registry.")
         handler = self.registry[type(query_spec)]
