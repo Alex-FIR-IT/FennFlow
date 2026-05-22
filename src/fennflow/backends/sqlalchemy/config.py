44290574bb7178c4fde50f0443182588e9ffda02
admin <osintfir@gmail.com>
2026-05-22 18:23:26 +0300

    feat: create a database_url_factory

--- src/fennflow/backends/sqlalchemy/config.py
+++ src/fennflow/backends/sqlalchemy/config.py
@@ -3,6 +3,23 @@ from pydantic import Field
 from fennflow.backends._abstract.config import AbstractBackendConfig
 
 
+def database_url_factory() -> str:
+    """Factory for database URL.
+
+    Returns: str - representing aiosqlite URL
+    Raises: ValueError - if aiosqlite is not installed
+    """
+    try:
+        import aiosqlite  # noqa: F401
+    except ImportError as _import_error:
+        raise ValueError(
+            "You need to pass database_url explicitly or "
+            "install aiosqlite first - `pip install aiosqlite`"
+        ) from _import_error
+
+    return "sqlite+aiosqlite:///fennflow.db"
+
+
 class SqlalchemyBackendConfig(AbstractBackendConfig):
     """Configuration for the SqlAlchemy backend.
 
@@ -11,7 +28,7 @@ class SqlalchemyBackendConfig(AbstractBackendConfig):
     """
 
     database_url: str = Field(
-        default="sqlite+aiosqlite:///fennflow.db",
+        default_factory=database_url_factory,
         description="Database URL for the FennFlow database",
     )
     db_schema: str = Field(
