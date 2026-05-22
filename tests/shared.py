830598fb5ac83353c2d6e6598a6ff57367fbf4af
admin <osintfir@gmail.com>
2026-05-22 23:47:35 +0300

    chore: change default scope

--- tests/shared.py
+++ tests/shared.py
@@ -6,4 +6,4 @@ if TYPE_CHECKING:
     from fennflow._new_types import BackendScope, Namespace
 
 NAMESPACE: Namespace = "user_files"
-SCOPE: BackendScope = "default_scope"
+SCOPE: BackendScope = "fennflow_backend"
