5a53686ad076a1c0f918aa58fdd66ee72485a2ea
admin <osintfir@gmail.com>
2026-05-22 16:59:15 +0300

    chore: delete redundant attribute

--- src/fennflow/connectors/_abstract/config.py
+++ src/fennflow/connectors/_abstract/config.py
@@ -1,15 +1,6 @@
-from pydantic import BaseModel, Field
 
 from fennflow._base_pydantic_config import BasePydanticConfig
 
 
 class AbstractConnectorConfig(BasePydanticConfig):
     """Abstract configuration for all FennFlow connectors."""
-
-    scope: BackendScope = Field(
-        default="default_scope",
-        description="Label to isolate backend state. "
-        "Useful when working with multiple storage instances "
-        "(e.g. two S3 or S3 and MinIO) "
-        "to avoid merging their files' metadata.",
-    )
