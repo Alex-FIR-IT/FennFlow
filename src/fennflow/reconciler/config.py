17318a36b050f43716601187557c9a32dec49d46
admin <osintfir@gmail.com>
2026-05-22 17:00:08 +0300

    feat: make ReconcileConfig convenient by allowing use literals instead of enums

--- src/fennflow/reconciler/config.py
+++ src/fennflow/reconciler/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from typing import Literal
 
 from fennflow._base_pydantic_config import BasePydanticConfig
 from fennflow.reconciler.enums import ReconcileFrequencyEnum, ReconcileStrategyEnum
@@ -7,6 +7,10 @@ from fennflow.reconciler.enums import ReconcileFrequencyEnum, ReconcileStrategyE
 class ReconcileConfig(BasePydanticConfig):
     """Configuration for the reconciler."""
 
-    frequency: ReconcileFrequencyEnum = ReconcileFrequencyEnum.ON_START_APP
-    strategy: ReconcileStrategyEnum = ReconcileStrategyEnum.FILL_IF_EMPTY
+    frequency: (
+        Literal["on_start_app", "on_session_start", "never"] | ReconcileFrequencyEnum
+    ) = ReconcileFrequencyEnum.ON_START_APP
+    strategy: (
+        Literal["fill_if_empty", "replace", "insert_missing"] | ReconcileStrategyEnum
+    ) = ReconcileStrategyEnum.FILL_IF_EMPTY
     batch_size: int = 1000
