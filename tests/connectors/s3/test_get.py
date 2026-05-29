8ee3e63f17725c90b0391dc2e2ac1fbea9e69f4d
admin <osintfir@gmail.com>
2026-05-29 21:09:38 +0300

    fix: fix a typo

--- tests/connectors/s3/test_get.py
+++ tests/connectors/s3/test_get.py
@@ -7,6 +7,6 @@ async def test_get_returns_empty_response_for_non_existing_file(uow_cls):
     async with uow_cls() as uow:
         response = await uow.connector.get(
             storage_path="fdf",
-            repo_extra=uow.files.repo_extra,
+            repo_extra=uow.user_files.repo_extra,
         )
         assert len(response) == 0
